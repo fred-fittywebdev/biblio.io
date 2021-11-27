fe107db41211af634fb94eb88b739c7beee7d06a
fred-fittywebdev <contact@fittywebdev.fr>
2021-11-27 15:48:49 +0100

    mise en place des routes pour le server

--- siteServeur/serveur.js
+++ siteServeur/serveur.js
@@ -3,6 +3,17 @@ let server = express()
 
 server.listen(8080)
 
-server.use('/', function() {
-    console.log('demande reçue');
+server.get('/', (requete, reponse) => {
+    console.log("Demande reçue sur la route / avec la méthode GET");
+    reponse.end('Demande GET reçue !')
+})
+
+server.get('/test', (requete, reponse) => {
+    console.log("Demande reçue sur la route /test avec la méthode GET");
+    reponse.end('Demande GET reçue !')
+})
+
+server.post('/test', (requete, reponse) => {
+    console.log("Demande reçue sur la route /test avec la méthode POST");
+    reponse.end('Demande POST reçue !')
 })
