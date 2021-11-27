77c7eaf05084123b97ec7a09cc5f821b52d6d20a
fred-fittywebdev <contact@fittywebdev.fr>
2021-11-27 15:16:01 +0100

    fin de la création de la bibliothèque, coté client uniquement, unisquement en js

--- js/main.js
+++ js/main.js
@@ -21,21 +21,27 @@ let livre4 = {
     "Pages": 1000
 }
 let livres = [livre1, livre2, livre3, livre4]
+afficherLivres()
 
-for (let i = 0; i < livres.length; i++) {
-    let livre1 = document.createElement('tr')
-    livre1.innerHTML = `
-        <td>${livres[i].Nom}</td>
-        <td>${livres[i].Auteur}</td>
-        <td>${livres[i].Pages}</td>
-        <td><button class="btn btn-warning">Modifier</button></td>
-        <td><button class="btn btn-danger">Supprimer</button></td>
-        `
-    table.appendChild(livre1)
+function afficherLivres() {
+    let tableauLivres = document.querySelector('#tableauLivres tbody')
+    let biblio = ""
+    for (let i = 0; i < livres.length; i++) {
+        biblio += `
+        <tr>
+            <td>${livres[i].Nom}</td>
+            <td>${livres[i].Auteur}</td>
+            <td>${livres[i].Pages}</td>
+            <td><button class="btn btn-warning" onClick="afficherForpModif(${i})">Modifier</button></td>
+            <td><button class="btn btn-danger" onClick="supprimerLivre(${i})">Supprimer</button></td>
+        </tr>    `
+    }
+    tableauLivres.innerHTML = biblio
 }
 
 function ajouterFormulaire() {
     document.querySelector('#formAjout').removeAttribute("class")
+    document.querySelector('#modifLivre').className = 'd-none'
 }
 
 document.querySelector('#validationAjout').addEventListener('click', function(e) {
@@ -44,6 +50,9 @@ document.querySelector('#validationAjout').addEventListener('click', function(e)
     let auteur = document.querySelector('#formAjout #auteur').value
     let pages = document.querySelector('#formAjout #pages').value
     ajoutLivre(titre, auteur, pages)
+    const formulaire = document.querySelector('#formAjout')
+    formulaire.reset()
+    formulaire.className = 'd-none'
 })
 
 function ajoutLivre(titre, auteur, pages) {
@@ -53,6 +62,41 @@ function ajoutLivre(titre, auteur, pages) {
         "Pages": pages
     }
     livres.push(livre)
-    console.log(livres)
+    afficherLivres()
 }
+
+function supprimerLivre(position) {
+    if(confirm("Ce livre sera définitivement supprimer, confirmez-vous?")) {
+        livres.splice(position, 1)
+        afficherLivres()
+        alert('Suprression effectuée')
+    } else {
+        alert('Suprression annulée')
+    }
+}
+
+function afficherForpModif(position) {
+    document.querySelector("#formAjout").className = 'd-none'
+    document.querySelector('#modifLivre').removeAttribute("class")
+    document.querySelector('#modifLivre #titre').value = livres[position].Nom
+    document.querySelector('#modifLivre #auteur').value = livres[position].Auteur
+    document.querySelector('#modifLivre #pages').value = livres[position].Pages
+    document.querySelector('#modifLivre #identifiant').value = position
+    console.log(position);
+}
+
+document.querySelector('#validationModif').addEventListener('click', function(e) {
+    e.preventDefault()
+    let titre = document.querySelector('#modifLivre #titre').value 
+    let auteur = document.querySelector('#modifLivre #auteur').value 
+    let pages = document.querySelector('#modifLivre #pages').value 
+    let positionLivre = document.querySelector('#modifLivre #identifiant').value 
+
+    livres[positionLivre].Nom = titre
+    livres[positionLivre].Auteur = auteur
+    livres[positionLivre].Pages = pages
+    afficherLivres()
+    document.querySelector('#modifLivre').className = 'd-none'
+})
+
 
